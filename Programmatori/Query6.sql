ae19109c5847bd7ae38229d2747773d76fd177dc
GiorgioBri <giorgio.brignoli42@gmail.com>
2022-05-03 13:22:51 +0200

    Query finite(?)

--- Programmatori/Query6.sql
+++ Programmatori/Query6.sql
@@ -2,6 +2,7 @@
 numero di programmi scritti da quel programmatore in quell'anno,
 con codice programmatore, anno e numero programmi*/
 
-select  
-from 
-where
+select Autore.Codice, Programma.Anno, count(Programma.ID)
+from Autore join Programmatore on Autore.Codice = Programmatore.Codice
+    join Programma on Autore.ID = Programma.ID
+group by Autore.Codice, Programma.Anno
--- Programmatori/Query7.sql
+++ Programmatori/Query7.sql
@@ -1,6 +1,7 @@
 /*Per ogni linguaggio:
 quanti sono in media gli autori dei programmi scritti in quel linguaggio*/
 
-select 
-from
-where 
+select Programma.Linguaggio, count(Autore.Codice)
+from Autore join Programmatore on Autore.Codice = Programmatore.Codice
+    join Programma on Autore.ID = Programma.ID
+group by Programma.Linguaggio
