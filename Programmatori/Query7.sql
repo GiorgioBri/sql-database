d80b967887aa8bb939c84e9cc548600074d9118b
GiorgioBri <giorgio.brignoli42@gmail.com>
2022-05-03 16:25:29 +0200

    Forse query 7 corretta (?)

--- Programmatori/Query7.sql
+++ Programmatori/Query7.sql
@@ -1,7 +1,7 @@
 /*Per ogni linguaggio:
 quanti sono in media gli autori dei programmi scritti in quel linguaggio*/
 
-select Programma.Linguaggio, count(Autore.Codice)
+select Programma.Linguaggio, count(Autore.Codice) / count(distinct Programma.ID)
 from Autore join Programmatore on Autore.Codice = Programmatore.Codice
     join Programma on Autore.ID = Programma.ID
 group by Programma.Linguaggio
