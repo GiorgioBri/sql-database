01100b070aac9bbc2c8fabb82f238cb6470cfaa0
GiorgioBri <giorgio.brignoli42@gmail.com>
2022-05-04 10:11:20 +0200

    Modifiche finali (?) 2

--- Programmatori/Query7.sql
+++ Programmatori/Query7.sql
@@ -2,6 +2,5 @@
 quanti sono in media gli autori dei programmi scritti in quel linguaggio*/
 
 select Programma.Linguaggio, count(Autore.Codice) / count(distinct Programma.ID) as Media
-from Autore join Programmatore on Autore.Codice = Programmatore.Codice
-    join Programma on Autore.ID = Programma.ID
+from Autore right join Programma on Autore.ID = Programma.ID
 group by Programma.Linguaggio
