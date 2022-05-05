93043f73b5d95d7ab421e20a88a137217c391dfb
GiorgioBri <giorgio.brignoli42@gmail.com>
2022-05-05 10:20:33 +0200

    Query 8

--- Scalatori/Query4.sql
+++ Scalatori/Query4.sql
@@ -1,7 +1,7 @@
 /*Per ogni nazione, calcola numero di scalate effettuate da 
 scalatore nati in quella nazione*/
 
-select Nazione.Nome, count(Nazione.Nome) as N°Scalate
+select Nazione.Nome, count(*) as N°Scalate
 from Scalatore join Scalata on Scalatore.CF = Scalata.Scalatore
     join Nazione on Nazione.Nome = Scalata.Nazione
 where Scalatore.NazioneNascita = Scalata.Nazione
--- Scalatori/Query8.sql
+++ Scalatori/Query8.sql
@@ -1,9 +1,7 @@
 /*Per ogni nazione N, calcola numero medio di scalate effettuate
 all'anno in N da scalatori nati in nazioni diverse da N*/
 
-select avg(Scalata.Anno)
+select Scalata.nazione, count(*)/count(Scalata.Anno) as Media
 from Scalatore join Scalata on Scalatore.CF = Scalata.Scalatore
-    join Nazione on Nazione.Nome = Scalata.Nazione
-where
+where Scalatore.NazioneNascita != Scalata.Nazione
 group by Scalata.Nazione
-having Scalatore.NAzioneNascita != Scalata.Nazione
